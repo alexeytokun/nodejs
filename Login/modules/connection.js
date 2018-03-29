e6f66c452821691b86502d5387d40f7c65bc2387
alexeytokun <alexeytokun@gmail.com>
2018-03-29 14:39:36 +0300

    recreate modules db, routes, connection

---
+++ Login/modules/connection.js
@@ -0,0 +1,10 @@
+var connectionObj = {
+    host: 'localhost',
+    user: 'root',
+    password: '123',
+    database: 'newdb',
+    connectionLimit: 100,
+    port: 8000
+};
+
+module.exports = connectionObj;
